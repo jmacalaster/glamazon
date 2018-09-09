4d69c07801d946915b5a46483ede5b30c41141c3
JMac <julia@defmethod.com>
2018-09-09 19:38:26 -0400

    Updates to Glamazon including updates to the database based on user input

--- glamazonCustomer.js
+++ glamazonCustomer.js
@@ -28,7 +28,7 @@ connection.connect(function(err) {
   showInventory();
 });
   
-//For the function run inventory, first create and show a table of all of the inventory in the database 
+//For the function show inventory, first create and show a table of all of the inventory in the database 
 
 function showInventory() {
   var query = "SELECT product_id, product_name, department_name, price, stock_quantity FROM inventory";
@@ -37,10 +37,12 @@ function showInventory() {
       console.log("Product ID: " + res[i].product_id + " || Name: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: $" + res[i].price + " || Available Inventory: " + res[i].stock_quantity);
     };
   });
-    inquirer.prompt(questions, processAnswers);
+  pickaProduct();
 };
 
 //Run inquirer to see what product the consumer would like to purchase and how many items they would like to purchase 
+//This function still works in the terminal, but has a bug where the question is asked before the inventory is 
+//shown on the terminal. The user can still enter a number and the question will then bump back down to the lowest line
 
 var questions = [
   {
@@ -56,8 +58,27 @@ var questions = [
 
 ];
 
-function processAnswers(answers){
-  console.log("And your answers were: " + answers);
-};
+function pickaProduct(){
+  inquirer.prompt(questions)
+  .then(function(answer){
+    var query2 = "SELECT stock_quantity, product_name FROM inventory WHERE ?";
+    connection.query(query2, { product_id: answer.ProductID }, function(err, res) {
+      if (res[0].stock_quantity >= answer.ProductQuant){
+        console.log("Purchase sucessful! You just purchased " + answer.ProductQuant + " " + res[0].product_name + "s");
+      }
+    var updatedQuantity = parseInt(res[0].stock_quantity -= answer.ProductQuant);
+    updateInventory(updatedQuantity);
+    });
+    function updateInventory(updatedQuantity){
+      var query3 = "UPDATE inventory SET stock_quantity = " + updatedQuantity + "WHERE product_id=" + answer.ProductID;
+    connection.query(query3, function(err, result) {
+      if (err) throw err;
+        console.log(result.affectedRows + " records updated");
+      });
+        console.log(answer.ProductID);
+        console.log(updatedQuantity);
+    }
+  });
+}
 
 
